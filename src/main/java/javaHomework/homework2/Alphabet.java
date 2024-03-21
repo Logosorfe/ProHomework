e108adbe2bb29158c0efa89d4f617a4dbd2bca68
lapan1ch <vitamin.lapickis@gmail.com>
2024-03-21 21:44:21 +0200

    Go

--- src/main/java/javaHomework/homework2/Alphabet.java
+++ src/main/java/javaHomework/homework2/Alphabet.java
@@ -3,10 +3,6 @@ package javaHomework.homework2;
 public class Alphabet {
     private String name;
 
-    public String getName() {
-        return name;
-    }
-
     public void setName(String name) {
         this.name = name;
     }
