d10d57a2982a7a74bc3ca7653ca7980b238c8ded
lapan1ch <vitamin.lapickis@gmail.com>
2024-05-31 10:23:42 +0200

    Let's go!

--- src/main/java/JavaHomework/homework14/TaskOne.java
+++ src/main/java/JavaHomework/homework14/TaskOne.java
@@ -1,16 +1,21 @@
 package JavaHomework.homework14;
 
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.nio.charset.StandardCharsets;
 
 public class TaskOne {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         String file = "C:/Users/logos/IdeaProjects/ProHomework/src/main/java/JavaHomework/homework14/testfile.txt";
-        FileOutputStream fileOutputStream = new FileOutputStream(file);
         String text = "This is my first experience when I myself work with IO API. I can do everything!";
 
-        byte[] textToByte = text.getBytes(StandardCharsets.UTF_8);
-        for(byte unit : textToByte) fileOutputStream.write(unit);
-        fileOutputStream.close();
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+            byte[] textToByte = text.getBytes(StandardCharsets.UTF_8);
+            for (byte unit : textToByte) fileOutputStream.write(unit);
+            fileOutputStream.close();
+        } catch (Exception e) {
+            e.getMessage();
+        }
     }
 }
