ac78f3d9b96f987475dc94b2671bffa7bb257e94
Logosorfe <logosorfe@gmail.com>
2025-01-09 16:09:51 +0200

    Let's go!

--- src/main/java/javaHomework/homework16/UniApp.java
+++ src/main/java/javaHomework/homework16/UniApp.java
@@ -4,13 +4,16 @@ public class UniApp {
 
     public static void main(String[] args) {
         Uni uni = new Uni();
-        uni.addStudent(new Student(1, "John", 23, Degree.BACHELOR));
-        uni.addStudent(new Student(2, "Jane", 21, Degree.MASTER));
-        uni.addStudent(new Student(3, "Dexter", 19, Degree.MASTER));
-        uni.addStudent(new Student(4, "Donald", 18, Degree.BACHELOR));
-        uni.addStudent(new Student(5, "Elon", 25, Degree.BACHELOR));
-
-        System.out.println(uni.getAllStudents());
-
+        try {
+            uni.addStudent(new Student(1, "John", 23, Degree.BACHELOR));
+            uni.addStudent(new Student(2, "Jane", 21, Degree.MASTER));
+            uni.addStudent(new Student(3, "Dexter", 19, Degree.MASTER));
+            uni.addStudent(new Student(4, "Donald", 18, Degree.BACHELOR));
+            uni.addStudent(new Student(5, "Elon", 25, Degree.BACHELOR));
+        }catch (UniversityIsFullException | UniqueStudentException | NullPointerException exception){
+            System.out.println(exception.getMessage());
+        }finally {
+            System.out.println(uni.getAllStudents());
+        }
     }
 }
